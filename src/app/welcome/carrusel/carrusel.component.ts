cfa1179897d65a57fdda793b903cc91822bd1238
1Don2angello <106407139+1Don2angello@users.noreply.github.com>
2023-08-17 15:45:57 -0500

    add imagens to welcome

--- src/app/welcome/carrusel/carrusel.component.ts
+++ src/app/welcome/carrusel/carrusel.component.ts
@@ -11,8 +11,21 @@ export class CarruselComponent  implements OnInit {
   public slides = [
     {
       id: 1,
-      name: "carlos",
+      name: "Ubica tu ruta",
+      subname: "Registre y mejora la calidad de tu movilidad",
       icon: "../../../assets/images/autobus.png"
+    },
+    { 
+      id: 2,
+      name: "Consulta los precios",
+      subname: "Registre y mejora la calidad de tu movilidad",
+      icon: "../../../assets/images/coin.png"
+    },
+    {
+      id: 3,
+      name: "Conoce los paraderos",
+      subname: "Registre y mejora la calidad de tu movilidad",
+      icon: "../../../assets/images/parada.png"
     }
   ]
   constructor() { }
