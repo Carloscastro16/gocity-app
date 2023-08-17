2cc2a6e89880d2dea89fd6183fa354f4846f2dc7
1Don2angello <106407139+1Don2angello@users.noreply.github.com>
2023-08-17 16:30:18 -0500

    delete json info

--- src/app/welcome/carrusel/carrusel.component.ts
+++ src/app/welcome/carrusel/carrusel.component.ts
@@ -12,19 +12,16 @@ export class CarruselComponent  implements OnInit {
     {
       id: 1,
       name: "Ubica tu ruta",
-      subname: "Registre y mejora la calidad de tu movilidad",
       icon: "../../../assets/images/autobus.png"
     },
     { 
       id: 2,
       name: "Consulta los precios",
-      subname: "Registre y mejora la calidad de tu movilidad",
       icon: "../../../assets/images/coin.png"
     },
     {
       id: 3,
       name: "Conoce los paraderos",
-      subname: "Registre y mejora la calidad de tu movilidad",
       icon: "../../../assets/images/parada.png"
     }
   ]
