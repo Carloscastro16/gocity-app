99fddbc4c27b34783038cf3c6c6a88a9ec216ac6
ccastro1016 <andycastro.001016@gmail.com>
2023-08-17 13:56:41 -0500

    Add routing

--- src/app/explore-container/explore-container.module.ts
+++ src/app/explore-container/explore-container.module.ts
@@ -5,10 +5,22 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { ExploreContainerComponent } from './explore-container.component';
+import { MapaComponent } from './components/mapa/mapa.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { TransportsComponent } from './components/transports/transports.component';
 
 @NgModule({
-  imports: [ CommonModule, FormsModule, IonicModule],
-  declarations: [ExploreContainerComponent],
+  imports: [ 
+    CommonModule, 
+    FormsModule, 
+    IonicModule
+  ],
+  declarations: [
+    ExploreContainerComponent,
+    MapaComponent,
+    ProfileComponent,
+    TransportsComponent,
+  ],
   exports: [ExploreContainerComponent]
 })
 export class ExploreContainerComponentModule {}
