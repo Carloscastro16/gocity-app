359c02adefb4e62917d9a7b158ea79e6cc896cbe
ccastro1016 <andycastro.001016@gmail.com>
2023-08-17 16:26:22 -0500

    Fix carrusel swiper styles

--- src/app/explore-container/components/transports/transports.component.ts
+++ src/app/explore-container/components/transports/transports.component.ts
@@ -26,6 +26,44 @@ export class TransportsComponent  implements OnInit {
       },
       score: "4"
     },
+    {
+      id: 2,
+      route: 'Ruta 1',
+      scheduleStart: '4:00',
+      scheduleEnd: '12:00',
+      stops: [
+        {
+          name: '1',
+          lat: 21.00000,
+          lng: 81.00000,
+        }
+      ],
+      base: {
+        name: 'Ruta 1',
+        lat: 21.00000,
+        lng: 81.00000,
+      },
+      score: "4"
+    },
+    {
+      id: 3,
+      route: 'Ruta 1',
+      scheduleStart: '4:00',
+      scheduleEnd: '12:00',
+      stops: [
+        {
+          name: '1',
+          lat: 21.00000,
+          lng: 81.00000,
+        }
+      ],
+      base: {
+        name: 'Ruta 1',
+        lat: 21.00000,
+        lng: 81.00000,
+      },
+      score: "4"
+    },
   ]
   constructor() { }
 
